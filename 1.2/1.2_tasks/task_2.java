ec57f82fde381c26eff97f7a0bcf1b21f17d77a0
andre <andrei.tsizikov00@gmail.com>
2026-01-14 13:44:02 +0200

    kotitehtava 1.2

--- 1.2/1.2_tasks/task_2.java
+++ 1.2/1.2_tasks/task_2.java
@@ -1,3 +1,23 @@
-void main() {
-    
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class task_2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Anna ensimmäinen kateeti");
+        int kateeti_1 = scanner.nextInt();
+
+        System.out.println("Anna toinen kateeti");
+        int kateeti_2 = scanner.nextInt();
+
+        // nopea ja yksinkertainen tapa
+        double hypotenuusa = Math.sqrt(kateeti_1 * kateeti_1 + kateeti_2 * kateeti_2);
+
+        // tapa missä käytetään potensia kuuten kaskimessa
+        double hypotenuusa_tapa_2 = Math.sqrt(Math.pow(kateeti_1, 2) + Math.pow(kateeti_2, 2));
+
+
+        System.out.println("kolmio hypotenuusan pitus on: " + hypotenuusa);
+    }
 }
--- 1.2/1.2_tasks/task_3.java
+++ 1.2/1.2_tasks/task_3.java
@@ -1,3 +1,24 @@
-void main() {
-    
+import java.util.Scanner;
+
+public class task_3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Weight (g): ");
+        double weight = scanner.nextDouble();
+
+        double gramsPerLuoti = 13.28;
+        double gramsPerNaula = 32 * gramsPerLuoti;
+        double gramsPerLeiviska = 20 * gramsPerNaula;
+
+        int leiviska = (int) (weight / gramsPerLeiviska);
+        double weightAfterLeiviska = weight - leiviska * gramsPerLeiviska;
+
+        int naula = (int) (weightAfterLeiviska / gramsPerNaula);
+        double weightAfterNaula = weightAfterLeiviska - naula * gramsPerNaula;
+
+        double luoti = weightAfterNaula / gramsPerLuoti;
+
+        System.out.printf((int) weight + " grams is " + leiviska + " leiviskä, " + naula + " naula, and %.2f%n", luoti, " luoti.");
+    }
 }
